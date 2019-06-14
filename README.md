bd7c942612a454519988586b8c34e1b4c3d43b1e
eamanu <eamanu@eamanu.com>
2019-06-13 22:54:22 -0300

    Fix untracked files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # itcrowd
 IT crowd  test
+
+# ISSUS / PR created meanwhile this test
+* https://github.com/dcrosta/flask-pymongo/pull/130
--- itcrowd/__init__.py
+++ itcrowd/__init__.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb://localhost:27017/itcrowddb"
 mongo = PyMongo(app)
+
+@app.route('/')
+def home():
+    test = mongo.db.test.insert_one({'test': 1234})
+    t = mongo.db.test.find({'test:': 1234})
+    print(t)
+    return render_template('index.html')
